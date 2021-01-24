e274bc916a41e96f16739dd478a94c0c567949a5
Andrea Bocci <andrea.bocci@cern.ch>
2021-01-25 00:04:43 +0100

    fixup! Replace boost::dynamic_bitset with a dedicated bitstream encoder

--- log2.h
+++ log2.h
@@ -1,6 +1,8 @@
 #ifndef log2_h
 #define log2_h
 
+#include <limits>
+
 template <typename T>
 constexpr T constexpr_log2(T value) {
   return value == 0 ? std::numeric_limits<T>::lowest() : value == 1 ? 0 : 1 + constexpr_log2(value / 2);
